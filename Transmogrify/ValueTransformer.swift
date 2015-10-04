34c957b9707bb0c94d681885874639645dbb4e87
Aron Cedercrantz <aron@cedercrantz.se>
2015-10-04 19:21:30 +0200

    Import project

---
+++ Transmogrify/ValueTransformer.swift
@@ -0,0 +1,109 @@
+//  Copyright © 2015 Aron Cedercrantz. All rights reserved.
+
+import Foundation
+
+public class ValueTransformer<A, B>: NSValueTransformer {
+
+    // MARK: - Value Transformation Block Types
+
+    /// The type of a block used to perform a forwad transform.
+    public typealias ForwardTransformer = (value: A?) -> B?
+    /// The type of a block used to perform a reverse transform.
+    public typealias ReverseTransformer = (value: B?) -> A?
+
+
+    // MARK: - Value Transformers
+
+    private let forwardTransformer: ForwardTransformer
+    private let reverseTransformer: ReverseTransformer?
+
+
+    // MARK: - Creating a New Value Transformer
+
+    /// Create a new value transformer using the given transformation block _forwardBlock_.
+    ///
+    /// - parameter forwardTransformer: A block used to perform a forward value transformation, that is
+    /// from value type `A?` to `B?`.
+    public class func forwardTransformer(forwardTransformer: ForwardTransformer) -> ValueTransformer<A, B> {
+        return ValueTransformer<A, B>(forwardTransformer: forwardTransformer, reverseTransformer: nil)
+    }
+
+    /// Create a new value transformer using the given transformation block _forwardBlock_.
+    ///
+    /// - parameter forwardTransformer: A block used to perform a forward value transformation, that is
+    /// from value type `A?` to `B?`.
+    /// - parameter reverseTransformer: A block used to perform a reverse value transformation, that is
+    /// from value type `B?` to `A?`.
+    public class func reversibleForwardTransformer(forwardTransformer: ForwardTransformer, reverseTransformer: ReverseTransformer) -> ValueTransformer<A, B> {
+        return ReversibleValueTransformer<A, B>(forwardTransformer: forwardTransformer, reverseTransformer: reverseTransformer)
+    }
+
+    /// Initialize a newly allocated value transformer with the given forward and reverse
+    /// transformation blocks.
+    ///
+    /// Note: You probably want to use on of the class functions instead to get a new instance;
+    /// namely `ValueTransformer.forwardTransformer()` or
+    /// `ValueTransformer.reversibleForwardTransformer(reverseTransformer:)`.
+    ///
+    /// - parameter forwardTransformer: A block used to perform a forward value transformation, that is
+    /// from value type `A?` to `B?`.
+    /// - parameter reverseTransformer: A block used to perform a reverse value transformation, that is
+    /// from value type `B?` to `A?`. This is an optional parameter.
+    public required init(forwardTransformer: ForwardTransformer, reverseTransformer: ReverseTransformer?) {
+        self.forwardTransformer = forwardTransformer
+        self.reverseTransformer = reverseTransformer
+    }
+
+
+    // MARK: - NSValueTransformer
+
+    public override class func transformedValueClass() -> AnyClass {
+        // We’d want to return `B.self` but I don’t know how to do that yet…
+        return NSObject.self
+    }
+
+    public override class func allowsReverseTransformation() -> Bool {
+        return false
+    }
+
+    /// Perform the forward value transform.
+    public func transformedValue(value: A?) -> B? {
+        return self.forwardTransformer(value: value)
+    }
+
+    func test() {
+        let pathToComponentsTransformer = ValueTransformer<String, [String]>.forwardTransformer({
+            if let value = $0 {
+                return value.componentsSeparatedByString("/")
+            }
+            return nil
+        })
+
+
+        let pathComponents = pathToComponentsTransformer.transformedValue("foo/bar/baz")
+        print(pathComponents)
+    }
+
+}
+
+
+// MARK: - ReversibleValueTransformer Class
+
+/// Any ValueTransformer supporting reverse transformation. Necessary because
+/// `allowsReverseTransformation` is a class function.
+public class ReversibleValueTransformer<A, B>: ValueTransformer<A, B> {
+
+    public required init(forwardTransformer: ForwardTransformer, reverseTransformer: ReverseTransformer?) {
+        assert(reverseTransformer != nil, "AlimentoCore.ReversibleValueTransformer requires the given _reverseBlock_ to have some value (i.e. it must not be `nil`).")
+        super.init(forwardTransformer: forwardTransformer, reverseTransformer: reverseTransformer)
+    }
+
+    public override class func allowsReverseTransformation() -> Bool {
+        return true
+    }
+
+    public func reverseTransformedValue(value: B?) -> A? {
+        return self.reverseTransformer!(value: value)
+    }
+    
+}
---
+++ TransmogrifyTests/TransmogrifyTests.swift
@@ -0,0 +1,36 @@
+//
+//  TransmogrifyTests.swift
+//  TransmogrifyTests
+//
+//  Created by Aron Cedercrantz on 04/10/15.
+//  Copyright © 2015 Aron Cedercrantz. All rights reserved.
+//
+
+import XCTest
+@testable import Transmogrify
+
+class TransmogrifyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
